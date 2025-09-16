8c542597f9822533a41848aac5a5279313da4b58
Thaysa Miyasak Rezende <thaysaif2008@gmail.com>
2025-09-16 15:09:01 -0400

    Add files via upload

---
+++ Database.php
@@ -0,0 +1,46 @@
+<?php
+require_once "config.php";
+class Database{
+    private $coon;
+    public function __construct(){
+        try{
+            $this->conn = new PDO("mysql:host".DB_HOST, DB_USER, DB_PASS);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+
+            $this->conn->exec("CREATE DATABASE IF NOT EXISTS ".DB_NAME);
+
+            $this->conn = new PDO("mysql:host=" .DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS);
+
+        } catch (PDOException $e){
+            die("ERRO de conexão: ". $e->getMessage());
+        }
+    }
+    
+
+public function criarTabela($comando){
+    try {
+        $sql = "CREATE TABLE IF NOT EXISTS $comando";
+        $this->conn->exec($sql);
+        echo "Tabela criada/verificada com sucesso.<br>";
+    } catch (PDOException $e) {
+        echo "Erro ao criar tabela" . $e->getMessage();
+    }
+}
+public function inserir($tabela, $dados) {
+    $colunas = implode(", ", array_keys($dados));
+    $placeholders = ":" . implode (", :", array_keys($dados));
+
+    $sql = "INSERT INTO $tabela (colunas) VALUES ($placeholders)";
+    $stmt = $this->conn->prepare($sql);
+    $stmt->execute($dados);
+}
+}
+$db = new Database();
+$db->criarTabela("usuários(id int 
+AUTO_INCREMENT PRIMARY KEY, 
+nome VARCHAR(100) NOT NULL,
+email VARCHAR(100) UNIQUE NOT NULL
+)");
+
+?>
---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+//Configurações do banco
+define("DB_HOST", "localhost");
+define("DB_USER", "root");//Nome do usuário do MySQL
+define("DB_PASS", "senha"); //Senha do MySQL
+define("DB_NAME", "meu_banco"); //Nome do banco a ser criado
+
+//var_dump(DB_HOST....)
+?>
